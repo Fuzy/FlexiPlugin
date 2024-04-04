951679b4cf75963b0f4ac4aa2a16663445488b28
mkadlec <martinkadlec2@gmail.com>
2024-04-04 12:23:33 +0200

    Akce hleda ve volani metody WSBundle#getTitle

--- src/main/java/usage/PsiNavigationDemoAction.java
+++ src/main/java/usage/PsiNavigationDemoAction.java
@@ -5,9 +5,7 @@ import bundle.XmlLoader;
 import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.diagnostic.Logger;
-import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
@@ -55,41 +53,33 @@ public class PsiNavigationDemoAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-        Editor editor = anActionEvent.getData(CommonDataKeys.EDITOR);
-        PsiFile psiFile = anActionEvent.getData(CommonDataKeys.PSI_FILE);
-        if (editor == null || psiFile == null) {
-            return;
-        }
-        int offset = editor.getCaretModel().getOffset();
 
-        final StringBuilder infoBuilder = new StringBuilder();
-        PsiElement element = psiFile.findElementAt(offset);
-        infoBuilder.append("Element at caret: ").append(element).append("\n");
-        if (element != null) {
-            PsiMethod containingMethod = PsiTreeUtil.getParentOfType(element, PsiMethod.class);
-            infoBuilder
-                    .append("Containing method: ")
-                    .append(containingMethod != null ? containingMethod.getName() : "none")
-                    .append("\n");
+        Project project = anActionEvent.getProject();
+
+        JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
+        PsiClass bundleClass = psiFacade.findClass("cz.winstrom.config.WSBundle", GlobalSearchScope.allScope(project));
+        if (bundleClass != null) {
+
+            PsiMethod[] containingMethods = PsiTreeUtil.getChildrenOfType(bundleClass, PsiMethod.class);
+
+            // cz.winstrom.config.WSBundle#getTitle(int, java.lang.String, java.lang.String, boolean, java.lang.Object...)
+            List<PsiMethod> collect = Arrays.stream(containingMethods)
+                    .filter(m -> "getTitle".equals(m.getName()))
+                    .filter(m -> m.getParameterList().getParameters().length == 5).toList();
+
+            if (collect.isEmpty()) {
+                LOG.error("Not found cz.winstrom.config.WSBundle#getTitle(int, java.lang.String, java.lang.String, boolean, java.lang.Object...)");
+                return;
+            }
+
+            if (collect.size() > 1) {
+                LOG.error("Unexpected count of cz.winstrom.config.WSBundle#getTitle() methods having 5 parameters");
+                return;
+            }
+
+            PsiMethod containingMethod = collect.get(0);
             if (containingMethod != null) {
-                PsiClass containingClass = containingMethod.getContainingClass();
-                infoBuilder
-                        .append("Containing class: ")
-                        .append(containingClass != null ? containingClass.getName() : "none")
-                        .append("\n");
-
-                infoBuilder.append("Local variables:\n");
-                containingMethod.accept(new JavaRecursiveElementVisitor() {
-                    @Override
-                    public void visitLocalVariable(@NotNull PsiLocalVariable variable) {
-                        super.visitLocalVariable(variable);
-                        infoBuilder.append(variable.getName()).append("\n");
-                    }
-                });
-
-                //TODO pouzije metodu kde je kurzor, nahradit za cz.winstrom.config.WSBundle#getTitle(int, java.lang.String, java.lang.String, boolean, java.lang.Object...)
-
-                Project project = anActionEvent.getProject();
+                LOG.warn("Starting method: " + containingMethod.getName() + " of class " + bundleClass.getName());
 
                 ModuleManager moduleManager = ModuleManager.getInstance(project);
                 Module[] allModules = moduleManager.getModules();
@@ -179,17 +169,6 @@ public class PsiNavigationDemoAction extends AnAction {
                     .collect(Collectors.toSet());
         }
     }
-
-    @Override
-    public void update(AnActionEvent e) {
-        Editor editor = e.getData(CommonDataKeys.EDITOR);
-        PsiFile psiFile = e.getData(CommonDataKeys.PSI_FILE);
-        e.getPresentation().setEnabled(editor != null && psiFile != null);
-    }
-
-
-    // dokud nemam vsechny pouziti s kompletnimi parametry
-    // seznam nezpracovanych PsiReference, relevantni L10nUsage a pracovni objekt: itemName schovani za promennou
 
     public List<L10nUsage> collectUsages(PsiMethod method, Collection<VirtualFile> files, Map<String, Integer> constants) {
 
